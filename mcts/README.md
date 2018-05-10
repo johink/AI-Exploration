aec6a9ab91990abef9f14b9faa9493f30a8e8825
Johink@gmail.com <johink@gmail.com>
2018-05-10 12:31:39 -0700

    mcts code has been merged into the willsmith repo

--- mcts/README.md
+++
@@ -1,5 +0,0 @@
-# Monte Carlo Tree Search
-
-An implementation and testing of the MCTS algorithm to improve my understanding of how it works, and utilizing it to plan moves in a simple nested tic tac toe game. 
-
-Work is being done along with [Chad Reynolds](https://github.com/cjreynol).
--- mcts/main.py
+++
@@ -1,8 +0,0 @@
-from agents.mcts_agent import MCTSAgent
-from agents.random_agent import RandomAgent
-from games.nested_ttt import NestedTTT
-from willsmith.simulator import Simulator
-
-if __name__ == "__main__":
-    simulator = Simulator(NestedTTT, [MCTSAgent, MCTSAgent])
-    simulator.run_game()
